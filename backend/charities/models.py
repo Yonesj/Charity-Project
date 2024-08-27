d10fb6decaa51266ff4c737e0e4867778f3db78e
Younes Jamshidi <jamshidiyounes92@gmail.com>
2024-08-27 20:24:01 +0330

    add Benefactor and Charity models

--- backend/charities/models.py
+++ backend/charities/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+class Benefactor(models.Model):
+    class BenefactorExperience(models.IntegerChoices):
+        BEGINNER = 0, 'Beginner'
+        INTERMEDIATE = 1, 'Intermediate'
+        EXPERT = 2, 'Expert'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    experience = models.SmallIntegerField(
+        choices=BenefactorExperience.choices,
+        default=BenefactorExperience.BEGINNER
+    )
+    free_time_per_week = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Charity(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
