2b6cde6165e1100509b33c9db2aeeab80f0bb202
Younes Jamshidi <jamshidiyounes92@gmail.com>
2024-08-27 20:13:03 +0330

    add custom User model and update settings

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    # inherited fields:
+    # id, date_joined, email, first_name, is_active, is_staff, is_superuser, last_login, last_name, username, password
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        UNSET = 'MF', 'Unset'
+
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.UNSET)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+
+    @property
+    def is_benefactor(self):
+        return hasattr(self, 'benefactor')
+
+    @property
+    def is_charity(self):
+        return hasattr(self, 'charity')
