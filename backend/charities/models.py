272921b6f5e06ac21f3d406e69247dac8115e77b
Younes Jamshidi <jamshidiyounes92@gmail.com>
2024-08-27 21:43:20 +0330

    add register-number validator

--- backend/charities/models.py
+++ backend/charities/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from accounts.models import User
+from .validators import RegNumberValidator
 
 
 class Benefactor(models.Model):
@@ -23,7 +24,7 @@ class Benefactor(models.Model):
 class Charity(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    reg_number = models.CharField(max_length=10)
+    reg_number = models.CharField(max_length=10, validators=[RegNumberValidator])
 
     def __str__(self):
         return self.name
