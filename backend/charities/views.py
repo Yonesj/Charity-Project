4b01ccc6e55e794cb89e479fd06a89868a2d5b3e
Younes Jamshidi <jamshidiyounes92@gmail.com>
2024-08-28 20:52:25 +0330

    add benefactor registration API

--- backend/charities/views.py
+++ backend/charities/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Benefactor
+from .serializers import BenefactorSerializer
+
+
+class BenefactorRegistrationAPI(generics.CreateAPIView):
+    queryset = Benefactor.objects.all()
+    serializer_class = BenefactorSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def create(self, request, *args, **kwargs):
+        serializer: BenefactorSerializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
