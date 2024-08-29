f0d09b8dca4e9877b414e39678caf2667212bac5
Younes Jamshidi <jamshidiyounes92@gmail.com>
2024-08-29 15:54:12 +0330

    add Tasks ListCreateAPI

--- backend/charities/views.py
+++ backend/charities/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from django.shortcuts import get_object_or_404
 
 from .models import Benefactor, Charity, Task
-from accounts.permissions import IsBenefactor
-from .serializers import BenefactorSerializer, CharitySerializer
+from accounts.permissions import IsBenefactor, IsCharityOwner
+from .serializers import BenefactorSerializer, CharitySerializer, TaskSerializer
 
 
 class BenefactorRegistration(generics.CreateAPIView):
@@ -35,8 +35,43 @@ class CharityRegistration(generics.CreateAPIView):
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class Tasks(APIView):
-    pass
+class Tasks(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.all_related_tasks_to_user(self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            **request.data,
+            "charity_id": request.user.charity.id
+        }
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            self.permission_classes = [IsAuthenticated, ]
+        else:
+            self.permission_classes = [IsCharityOwner, ]
+
+        return [permission() for permission in self.permission_classes]
+
+    def filter_queryset(self, queryset):
+        filter_lookups = {}
+        for name, value in Task.filtering_lookups:
+            param = self.request.GET.get(value)
+            if param:
+                filter_lookups[name] = param
+        exclude_lookups = {}
+        for name, value in Task.excluding_lookups:
+            param = self.request.GET.get(value)
+            if param:
+                exclude_lookups[name] = param
+
+        return queryset.filter(**filter_lookups).exclude(**exclude_lookups)
 
 
 class TaskRequest(APIView):
