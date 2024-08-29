b96c13d9b6557a766dcb998eda2aa5343c3f3806
Younes Jamshidi <jamshidiyounes92@gmail.com>
2024-08-29 12:22:23 +0330

    add CharityRegistration view

--- backend/charities/views.py
+++ backend/charities/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Benefactor
-from .serializers import BenefactorSerializer
+from .models import Benefactor, Charity
+from .serializers import BenefactorSerializer, CharitySerializer
 
 
-class BenefactorRegistrationAPI(generics.CreateAPIView):
+class BenefactorRegistration(generics.CreateAPIView):
     queryset = Benefactor.objects.all()
     serializer_class = BenefactorSerializer
     permission_classes = (IsAuthenticated,)
@@ -17,3 +18,32 @@ class BenefactorRegistrationAPI(generics.CreateAPIView):
             serializer.save(user=request.user)
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CharityRegistration(generics.CreateAPIView):
+    queryset = Charity.objects.all()
+    serializer_class = CharitySerializer
+    permission_classes = (IsAuthenticated,)
+
+    def create(self, request, *args, **kwargs):
+        serializer: CharitySerializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Tasks(APIView):
+    pass
+
+
+class TaskRequest(APIView):
+    pass
+
+
+class TaskResponse(APIView):
+    pass
+
+
+class DoneTask(APIView):
+    pass
