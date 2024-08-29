5021aa416b872497f9defd0a1da34a85265b2486
Younes Jamshidi <jamshidiyounes92@gmail.com>
2024-08-29 15:44:12 +0330

    add Task serializer

--- backend/charities/serializers.py
+++ backend/charities/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Benefactor, Charity
+from .models import Benefactor, Charity, Task
 
 
 class BenefactorSerializer(serializers.ModelSerializer):
@@ -20,3 +20,26 @@ class CharitySerializer(serializers.ModelSerializer):
     def save(self, **kwargs):
         user = kwargs.get('user')
         return super().save(user=user)
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    state = serializers.ChoiceField(read_only=True, choices=Task.TaskStatus.choices)
+    assigned_benefactor = BenefactorSerializer(required=False)
+    charity = CharitySerializer(read_only=True)
+    charity_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Charity.objects.all(), source='charity')
+
+    class Meta:
+        model = Task
+        fields = (
+            'id',
+            'title',
+            'state',
+            'charity',
+            'charity_id',
+            'description',
+            'assigned_benefactor',
+            'date',
+            'age_limit_from',
+            'age_limit_to',
+            'gender_limit',
+        )
